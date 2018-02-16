a19ac98dbc8cbc45943236fb141fcaa4a809bd3f
Adam Hansen <adamlars90@gmail.com>
2018-02-16 16:24:55 +0100

    Implemented routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
+import { AlbumsModule } from './albums/albums.module';
 
 
 @NgModule({
@@ -14,7 +16,9 @@ import {MatButtonModule} from '@angular/material';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule,
+    AlbumsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
