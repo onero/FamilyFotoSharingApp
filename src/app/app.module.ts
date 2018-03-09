27c0b9ec123ee3fd16c42109d67eb15b1622f128
Adam Hansen <adamlars90@gmail.com>
2018-03-09 21:27:24 +0100

    Added profile pic tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatListModule, MatSidenavModule} from '@angular/material';
+import {MatButtonModule, MatListModule, MatSidenavModule, MatTooltipModule} from '@angular/material';
 import {AppRoutingModule} from './app-routing.module';
 import {AlbumsModule} from './albums/albums.module';
 import {SharedModule} from './shared/shared.module';
